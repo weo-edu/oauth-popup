15a9ca581632eb5564f0df7a608dfab02bdb08fb
Andrew Shaffer <andrew@weo.io>
2016-10-05 17:57:01 -0700

    make it more compatible with older browsers

--- index.js
+++ index.js
@@ -1,51 +1,58 @@
-var wndFeatures = require('window-features');
+var wndFeatures = require('window-features')
 
-module.exports = function(url, config, done) {
-  var features = wndFeatures(config);
-  var wnd = window.open(url, '_blank', features);
-  wnd.focus();
+module.exports = function (url, config, done) {
+  var features = wndFeatures(config)
+  var wnd = window.open(url, '_blank', features)
+  wnd.focus()
 
-  window.addEventListener('message', function handler(evt) {
+  window.addEventListener('message', function handler (evt) {
     if(evt.origin === window.location.origin) {
-      wnd.close();
-      if(evt.data.error)
-        done(evt.data.error);
-      else
-        done(null, evt.data);
-
-      clearTimeout(interval);
-      window.removeEventListener('message', handler, false);
+      handleData(evt.data)
     }
-  }, false);
+  }, false)
 
-  var interval = setInterval(function() {
+  window.__oauthPopupRedirectFn = handleData
+
+  function handleData (data) {
+    wnd.close()
+    data.error ? done(data.error) : done(null, data)
+    clearTimeout(interval)
+    window.removeEventListener('message', handler, false)
+  }
+
+  var interval = setInterval(function () {
     if(wnd.closed) {
-      clearTimeout(interval);
-      done(new Error('Authorization failed'));
+      clearTimeout(interval)
+      done(new Error('Authorization failed'))
     }
-  }, 50);
-};
+  }, 50)
+}
 
-function parseQs(str) {
+function parseQs (str) {
   return str ? str.split('#')[0].split('&').reduce(function(memo, part) {
-    var parts = part.split('=');
-    memo[parts[0]] = decodeURIComponent(parts[1]);
-    return memo;
-  }, {}) : {};
+    var parts = part.split('=')
+    memo[parts[0]] = decodeURIComponent(parts[1])
+    return memo
+  }, {}) : {}
 }
 
 try {
-  var params = parseQs(window.location.search.substring(1));
+  var params = parseQs(window.location.search.substring(1))
 
   if (window.opener && window.opener.location.origin === window.location.origin) {
     if (params.oauth_token && params.oauth_verifier) {
-      window.opener.postMessage({oauth_token: params.oauth_token, oauth_verifier: params.oauth_verifier}, window.location.origin);
+      passData({oauth_token: params.oauth_token, oauth_verifier: params.oauth_verifier}, window.location.origin)
     } else if (params.code) {
-      window.opener.postMessage({code: params.code}, window.location.origin);
+      passData({code: params.code}, window.location.origin)
     } else if (params.error) {
-      window.opener.postMessage({error: params.error}, window.location.origin);
+      passData({error: params.error}, window.location.origin)
     }
+  }
+
+  function passData (data) {
+    (window.opener.__oauthPopupRedirectFn || window.opener.postMessage)(data)
   }
 } catch(ex) {
 
 }
+
